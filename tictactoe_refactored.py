849a34bc695df97344bbaa7e4b4dd8d26e55aec8
Kushal K S <kushalkishor31@gmail.com>
2024-07-11 10:21:36 +0000

    removed unnecessary boolean flag from tictactoe_refactored.py

--- tictactoe_refactored.py
+++ tictactoe_refactored.py
@@ -6,7 +6,6 @@ O = "O"
 current_grid = [1, 2, 3, 4, 5, 6, 7, 8, 9]  # Using numbers for initial visualization
 straight_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [3, 5, 7], [1, 5, 9]]
 occupied_pos = []
-is_game_over = False
 
 
 def display_grid():
